d1760f362397c7547fbe272bd7a2171ef0520148
Joseph Portaro <jsportaro@outlook.com>
2024-09-02 22:29:37 -0400

    Refactored to include and src directories

---
+++ count_words/include/counter.h
@@ -0,0 +1,6 @@
+#ifndef __COUNTER_H__
+#define __COUNTER_H__
+
+extern void counter(int counts[4]);
+
+#endif
---
+++ count_words/include/lexer.h
@@ -0,0 +1,7 @@
+#ifndef __LEXER_H__
+#define __LEXER_H__
+
+extern int fee_count, fie_count, foe_count, fum_count;
+extern int yylex(void);
+
+#endif
